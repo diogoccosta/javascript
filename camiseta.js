54a7f7a8370823757967adc1151c7a19dd0443d7
Diogo Carvalho <diogoccosta@icloud.com>
2019-06-19 15:03:23 -0300

    first

---
+++ camiseta.js
@@ -0,0 +1,22 @@
+//Usando //
+//Growth
+//Digital
+//Transformation
+Tango.Tech
+
+//Usando */
+/* Growth  
+Digital
+Transformation */
+Tango.Tech
+
+//Usando </>
+</>Growth
+</>Digital
+</>Transformation
+</>Tango.Tech
+
+//Enviado antes
+Growth&
+Digital&
+Transformation
---
+++ fundamentos/nullUnedfined.js
@@ -0,0 +1,6 @@
+let valor // não inicializada
+console.log(valor)
+//console.log(valor2) // nem foi declarada
+
+valor = null // ausência de valor
+console.log(valor)
